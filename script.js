320052676d6b261cd4a94f296126790c80d310a0
saianirudh-t <saianirudh2004@gmail.com>
2025-12-11 10:25:34 +0530

    search done

--- script.js
+++ script.js
@@ -42,16 +42,58 @@ async function loadContent(endpoint, container) {
     });
 }
 
+function changeHeading(list) {
+    const headings = document.querySelectorAll(".containerHeading")
+    list.forEach((section, index) => {
+        headings[index].textContent = section.heading
+    })
+}
+
+const searchInput = document.getElementById("searchInput")
+
+async function searchContent(query) {
+    trending.innerHTML = ""  
+    popular.innerHTML = ""  
+    toprated.innerHTML = ""  
+    if (query.trim() === "") {
+        loadCategory(moviesPage)
+        return
+    }
+    const url = `${baseUrl}/search/multi?api_key=${apiKey}&query=${query}`
+    const response = await fetch(url)
+    const data = await response.json()
+    data.results.forEach(item => {
+        if (!item.poster_path) return
+        let title = item.title || item.name
+        let card = document.createElement("div")
+        card.className = "movie-card"
+        card.innerHTML = `
+            <h2 class="movie-title">${title}</h2>
+            <img class="movie-poster" src="${imgBase + item.poster_path}">
+            <p class="overview">${item.overview || "No description available"}</p>
+            <button class="see-more">See More</button>
+            <p class="rating">⭐ ${item.vote_average}</p>
+        `
+
+        trending.appendChild(card)
+    })
+}
+
+searchInput.addEventListener("input", () => {
+    let query = searchInput.value
+    searchContent(query)
+})
+
 const moviesPage = [
-  { endpoint: "trending/movie/week", container: trending },
-  { endpoint: "movie/popular", container: popular },
-  { endpoint: "movie/top_rated", container: toprated }
+  { endpoint: "trending/movie/week", container: trending, heading:"Trending right now"},
+  { endpoint: "movie/popular", container: popular, heading:"Most popular among" },
+  { endpoint: "movie/top_rated", container: toprated, heading:"Top rated in IMDB" }
 ]
 
 const seriesPage = [
-  { endpoint: "trending/tv/week", container: trending },
-  { endpoint: "tv/popular", container: popular },
-  { endpoint: "tv/top_rated", container: toprated }
+  { endpoint: "trending/tv/week", container: trending,heading:"Trending right now"},
+  { endpoint: "tv/popular", container: popular,heading:"Most popular among" },
+  { endpoint: "tv/top_rated", container: toprated,heading:'Top rated in IMDB' }
 ]
 
 const upcomingPage=[
@@ -61,10 +103,12 @@ const upcomingPage=[
 ]
 
 function loadCategory(list) {
+    changeHeading(list)
     list.forEach(section => {
         loadContent(section.endpoint, section.container);
     });
 }
+
 
 loadCategory(moviesPage)
 
